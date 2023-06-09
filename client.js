07443354b5355478c02c32cfc9f5f1fb888fe298
nathaniaelirica <nathaniaelirica@gmail.com>
2023-06-09 19:12:54 +0700

    grpc-js

--- client.js
+++ client.js
@@ -1,15 +1,22 @@
-const grpc = require('grpc');
-const protoLoader = require('@grpc/proto-loader');
-const readline = require('readline');
+const grpc = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
+const readline = require("readline");
 
-// Memuat file proto gRPC
-const protoPath = './gate.proto';
-const packageDefinition = protoLoader.loadSync(protoPath);
-const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
-const gateService = protoDescriptor.gate.GateService;
+// Load gRPC proto file
+const protoPath = "./gate.proto";
+const packageDefinition = protoLoader.loadSync(protoPath, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
 
-
-const client = new gateService('localhost:50051', grpc.credentials.createInsecure());
+const gateProto = grpc.loadPackageDefinition(packageDefinition).gate;
+const client = new gateProto.GateService(
+  "localhost:50051",
+  grpc.credentials.createInsecure()
+);
 
 // Membuat interface untuk membaca input dari command line
 const rl = readline.createInterface({
@@ -18,15 +25,15 @@ const rl = readline.createInterface({
 });
 
 // Menampilkan pilihan antara "Masuk" atau "Keluar"
-console.log('Pilih layanan:');
-console.log('1. Masuk');
-console.log('2. Keluar');
-rl.question('Pilihan: ', (choice) => {
-  if (choice === '1') {
+console.log("Pilih layanan:");
+console.log("1. Masuk");
+console.log("2. Keluar");
+rl.question("Pilihan: ", (choice) => {
+  if (choice === "1") {
     // Meminta input ID kartu dari pengguna
-    rl.question('Masukkan ID kartu: ', (idKartuAkses) => {
+    rl.question("Masukkan ID kartu: ", (idKartuAkses) => {
       // Meminta input ID register dari pengguna
-      rl.question('Masukkan ID gate: ', (idRegisterGate) => {
+      rl.question("Masukkan ID gate: ", (idRegisterGate) => {
         // Membuat request ke service "Masuk"
         const request = {
           idKartuAkses: idKartuAkses,
@@ -36,14 +43,14 @@ rl.question('Pilihan: ', (choice) => {
         // Melakukan pemanggilan gRPC ke server
         client.masuk(request, (err, response) => {
           if (err) {
-            console.error('Error:', err);
+            console.error("Error:", err);
           } else {
-            console.log('Response:', response);
+            console.log("Response:", response);
             // Lakukan tindakan sesuai dengan response yang diterima
             if (response.response === 1) {
-              console.log('Berhasil masuk');
+              console.log("Berhasil masuk");
             } else {
-              console.log('Gagal masuk');
+              console.log("Gagal masuk");
             }
           }
 
@@ -53,39 +60,39 @@ rl.question('Pilihan: ', (choice) => {
         });
       });
     });
-  } else if (choice === '2') {
-   // Meminta input ID kartu dari pengguna
-   rl.question('Masukkan ID kartu: ', (idKartuAkses) => {
-    // Meminta input ID register dari pengguna
-    rl.question('Masukkan ID gate: ', (idRegisterGate) => {
-      // Membuat request ke service "Masuk"
-      const request = {
-        idKartuAkses: idKartuAkses,
-        idRegisterGate: idRegisterGate,
-      };
+  } else if (choice === "2") {
+    // Meminta input ID kartu dari pengguna
+    rl.question("Masukkan ID kartu: ", (idKartuAkses) => {
+      // Meminta input ID register dari pengguna
+      rl.question("Masukkan ID gate: ", (idRegisterGate) => {
+        // Membuat request ke service "Masuk"
+        const request = {
+          idKartuAkses: idKartuAkses,
+          idRegisterGate: idRegisterGate,
+        };
 
-      // Melakukan pemanggilan gRPC ke server
-      client.keluar(request, (err, response) => {
-        if (err) {
-          console.error('Error:', err);
-        } else {
-          console.log('Response:', response);
-          // Lakukan tindakan sesuai dengan response yang diterima
-          if (response.response === 1) {
-            console.log('Berhasil keluar');
+        // Melakukan pemanggilan gRPC ke server
+        client.keluar(request, (err, response) => {
+          if (err) {
+            console.error("Error:", err);
           } else {
-            console.log('Gagal keluar');
+            console.log("Response:", response);
+            // Lakukan tindakan sesuai dengan response yang diterima
+            if (response.response === 1) {
+              console.log("Berhasil keluar");
+            } else {
+              console.log("Gagal keluar");
+            }
           }
-        }
 
-        // Menutup interface readline dan mengakhiri program
-        rl.close();
-        process.exit(0);
+          // Menutup interface readline dan mengakhiri program
+          rl.close();
+          process.exit(0);
+        });
       });
     });
-  });
   } else {
-    console.log('Pilihan tidak valid');
+    console.log("Pilihan tidak valid");
     // Menutup interface readline dan mengakhiri program
     rl.close();
     process.exit(0);
